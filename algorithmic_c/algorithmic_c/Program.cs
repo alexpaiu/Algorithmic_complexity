1c3468a8ee2a478debc6856977852c6bc1235a6e
alexpaiu <alex.paiu23@gmail.com>
2019-09-26 11:04:37 +0100

    first commit

---
+++ algorithmic_c/algorithmic_c/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algorithmic_c
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
